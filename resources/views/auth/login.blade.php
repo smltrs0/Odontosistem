727fa096b8072bfc688df1262d1c6da1fa5c0828
Samuel <smltrs0@gmail.com>
2020-08-25 21:21:01 -0430

    config change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Odontosistem') }}</title>
     <link href="{{ asset('css/base.css') }}" rel="stylesheet">
+
 </head>
 <body >
     <div>
